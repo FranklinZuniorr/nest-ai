fd3bc3ee232bc8d46834dde4e29b4088795f0ca8
Franklin Vieira <franklinbarbs763@gmail.com>
2023-04-27 16:47:30 -0300

    Start project.

---
+++ src/gpt/ai.controller.ts
@@ -0,0 +1,23 @@
+import { Controller, Post, Body, Get, Param, HttpStatus, NotFoundException } from '@nestjs/common';
+import { NestResponse } from '../core/http/nest-response';
+import { NestResponseBuilder } from '../core/http/nest-response-builder';
+import { AiService } from './ai.service';
+import { Ai } from './ai.entity';
+
+@Controller('ai')
+export class AiController {
+
+    constructor(private aiService: AiService) {}
+
+    @Post()
+    public async cria(@Body() text: Ai): Promise <NestResponse> {
+        
+        const data = await this.aiService.solicitarAi(text);
+
+
+        return new NestResponseBuilder()
+                .comStatus(HttpStatus.CREATED)
+                .comBody(data)
+                .build();
+    }
+}
---
+++ src/gpt/ai.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@nestjs/common';
+import { Ai } from './ai.entity';
+const axios = require("axios");
+require("dotenv").config();
+
+@Injectable()
+export class AiService {
+
+    public solicitarAi(text: Ai): any {
+
+        const apiKey = process.env.OPENAI_API_KEY;
+        const baseURL = "https://api.openai.com/v1";
+
+        const openai = axios.create({
+        baseURL,
+        headers: {
+            Authorization: `Bearer ${apiKey}`,
+            "Content-Type": "application/json",
+        },
+        });
+
+        const prompt = text.msg;
+        const model = "text-davinci-003";
+        const maxTokens = 500;
+        const temperature = 1;
+
+        const data = {
+        prompt,
+        model,
+        max_tokens: maxTokens,
+        temperature,
+        };
+
+        const dataRes = openai
+        .post("/completions", data)
+        .then((response) => {
+            const answer = response.data.choices[0].text.trim();
+            return answer
+        })
+        .catch((error) => {
+            return error
+        });
+
+        return dataRes
+
+    }
+}
