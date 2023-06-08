2a523b9620e34631f2e5c4fbd6269ca95fabd951
Franklin Vieira <franklinbarbs763@gmail.com>
2023-06-08 20:18:17 -0300

    Att.

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,7 @@ async function bootstrap() {
     preflightContinue: false,
     optionsSuccessStatus: 204,
     credentials: true,
-    allowedHeaders: 'Content-Type, Accept, Authorization, accessToken, file, Access-Control-Allow-Origin',
+    allowedHeaders: 'Content-Type, Accept, Authorization, accessToken, file',
   });
 
   await app.listen(process.env.PORT || 3001);
