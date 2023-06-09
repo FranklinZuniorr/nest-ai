eeb887a1293354063b35b18536f1616508ab1720
Franklin Vieira <franklinbarbs763@gmail.com>
2023-06-08 21:05:15 -0300

    Att.

--- src/common/filtros/filter-of-exception-http.filter.ts
+++ src/common/filtros/filter-of-exception-http.filter.ts
@@ -1,48 +1,44 @@
 import { Catch, ExceptionFilter, ArgumentsHost, HttpException, HttpStatus } from "@nestjs/common";
-import { HttpAdapterHost, AbstractHttpAdapter } from "@nestjs/core";
+import { Response } from "express";
 
 @Catch()
 export class FilterOfExceptionHttp implements ExceptionFilter {
+  catch(exception: any, host: ArgumentsHost) {
+    const contexto = host.switchToHttp();
+    const resposta = contexto.getResponse<Response>();
 
-    private httpAdapter: AbstractHttpAdapter;
+    // Configurações do cabeçalho CORS
+    resposta.header('Access-Control-Allow-Origin', '*');
+    resposta.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS');
+    resposta.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
 
-    constructor(adapterHost: HttpAdapterHost) {
-        this.httpAdapter = adapterHost.httpAdapter;
-    }
+    const status =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR;
 
-    catch(exception: Error, host: ArgumentsHost) {
-        const contexto = host.switchToHttp();
-        const requisicao = contexto.getRequest();
-        const resposta = contexto.getResponse();
+    const body =
+      exception instanceof HttpException
+        ? {
+            r: false,
+            data: {
+              error: exception.getResponse(),
+              msg: JSON.parse(JSON.stringify(exception.getResponse())).message,
+            },
+            status: exception.getStatus(),
+          }
+        : {
+            r: false,
+            data: {
+              statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+              timestamp: new Date().toISOString(),
+              message: exception.message,
+              path: contexto.getRequest().path,
+              msg: exception.message,
+            },
+            status: HttpStatus.INTERNAL_SERVER_ERROR,
+          };
 
-        const { status, body } = exception instanceof HttpException 
-            ? {
-                status: exception.getStatus(),
-                body: {
-                    r: false, 
-                    data: {
-                        error: exception.getResponse(), 
-                        msg: JSON.parse(JSON.stringify(exception.getResponse())).message
-                    }, 
-                    status: exception.getStatus()
-                }
-            }
-            : {
-                status: HttpStatus.INTERNAL_SERVER_ERROR,
-                body: {
-                    r: false,
-                    data: {
-                        statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
-                        timestamp: new Date().toISOString(),
-                        message: exception.message,
-                        path: requisicao.path,
-                        msg: exception.message
-                    },
-                    status: HttpStatus.INTERNAL_SERVER_ERROR
-                }
-            };
-        
-        this.httpAdapter.reply(resposta, body, status);
-    }
-
+    resposta.status(status).json(body);
+  }
 }
