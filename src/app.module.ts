e14485d4d4c6da7388a2b94dcdc48cfa7da3451a
Franklin Vieira <franklinbarbs763@gmail.com>
2023-06-22 09:27:10 -0300

    Att.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module, ClassSerializerInterceptor } from '@nestjs/common';
+import { Module, ClassSerializerInterceptor, MiddlewareConsumer, RequestMethod } from '@nestjs/common';
 import { APP_INTERCEPTOR, APP_FILTER } from '@nestjs/core';
 import { UsuarioModule } from './usuario/user.module';
 import { FilterOfExceptionHttp } from './common/filtros/filter-of-exception-http.filter';
@@ -6,6 +6,7 @@ import { TransformResponseInterceptor } from './core/http/transform-response.int
 import { AiModule } from './gpt/ai.module';
 import { DatabaseModule } from './mongoDb/dataBase.module';
 import { PaymentModule } from './payment/payment.module';
+import * as bodyParser from 'body-parser';
 @Module({
   imports: [UsuarioModule, AiModule, DatabaseModule, PaymentModule],
   controllers: [],
@@ -24,4 +25,10 @@ import { PaymentModule } from './payment/payment.module';
     }
   ],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(bodyParser.raw({ type: 'application/json' }))
+      .forRoutes({ path: "/stripe/webhook", method: RequestMethod.POST });
+  }
+}
