df4aeb166d9ef3c188ee4b0aaef6263672e2e0cb
Franklin Vieira <franklinbarbs763@gmail.com>
2023-07-14 11:15:21 -0300

    Att.

--- src/usuario/user.entity.externalUrl.ts
+++ src/usuario/user.entity.externalUrl.ts
@@ -1,4 +1,4 @@
-import { IsNotEmpty, IsString, IsEmail, Matches, IsNotEmptyObject, isNotEmpty, IsObject, IsNumber } from 'class-validator';
+import { IsNotEmpty, IsString, IsEmail, Matches, IsNotEmptyObject, isNotEmpty, IsObject, IsNumber, isURL, IsUrl } from 'class-validator';
 import { Expose } from 'class-transformer';
 
 export class ExternalUrl {
@@ -12,5 +12,6 @@ export class ExternalUrl {
     @IsString({
         message: "ExternalUrl é uma string!"
     })
+    @IsUrl(undefined, {message: "Precisa ser um link!"})
     externalUrl: string;
 };
